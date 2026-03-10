5fac8ed0e48762fe27ba57436a37a58288c75568
Wushengyouhan <840835067@qq.com>
2026-03-10 10:58:52 +0800

    47. Conditional Rendering With &&

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -67,14 +67,17 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
         <h2>Our Menu</h2>
-        <ul className="pizzas">
+        {numPizzas > 0 &&(<ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
-        </ul>
+        </ul>)}
     </main>
   );
 }
@@ -100,7 +103,14 @@ function Footer() {
   console.log(isOpen);
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open!
+      {isOpen && (
+        <div className="order">
+          <p>
+            We're open until {closeHour}:00. Come visit us or order online.
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 
