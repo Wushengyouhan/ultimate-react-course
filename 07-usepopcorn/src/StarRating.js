40164ddb21608db21e654460a86c33cacc878182
Wushengyouhan <840835067@qq.com>
2026-04-02 09:00:53 +0800

    117. Handling Hover Events

--- 07-usepopcorn/src/StarRating.js
+++ 07-usepopcorn/src/StarRating.js
@@ -16,6 +16,7 @@ export default function StarRating({ maxRating = 5 }) {
   };
 
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   const handleRating = (rating) => {
     setRating(rating);
@@ -29,12 +30,14 @@ export default function StarRating({ maxRating = 5 }) {
             <Star
               key={i}
               onRate={() => handleRating(i + 1)}
-              full={i + 1 <= rating}
+              full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
             />
           </span>
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -46,9 +49,14 @@ const starStyle = {
   cursor: "pointer",
 };
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span style={starStyle} onClick={() => onRate()}>
+    <span
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
