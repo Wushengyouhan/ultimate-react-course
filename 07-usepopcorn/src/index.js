e3a24e2bb5c4a49df3e015920ed7e9add5a71078
Wushengyouhan <840835067@qq.com>
2026-04-21 10:03:20 +0800

    143. useEffect to the Rescue

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 import App from "./App";
-import StarRating from "./StarRating";
-import Test from "./Test";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Test />
+    <App />
   </React.StrictMode>,
 );
