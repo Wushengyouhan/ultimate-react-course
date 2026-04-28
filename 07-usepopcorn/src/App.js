b3d7bb36d23fefd5cdb842b5259c7f8003bafb93
Wushengyouhan <840835067@qq.com>
2026-04-28 10:24:43 +0800

    156. Cleaning Up Data Fetching

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -62,6 +62,7 @@ export default function App() {
   const [selectedId, setSelectedId] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
     async function fetchMovies() {
       try {
         setIsLoading(true);
@@ -69,6 +70,7 @@ export default function App() {
 
         const res = await fetch(
           `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          { signal: controller.signal },
         );
         if (!res.ok)
           throw new Error("Something went wrong with fetching movies");
@@ -76,7 +78,9 @@ export default function App() {
         if (data.Response === "False") throw new Error("Movie not found");
         setMovies(data.Search);
       } catch (err) {
-        setError(err.message);
+        if (err.name !== "AbortError") {
+          setError(err.message);
+        }
       } finally {
         setIsLoading(false);
       }
@@ -87,6 +91,8 @@ export default function App() {
       return;
     }
     fetchMovies();
+
+    return () => controller.abort();
   }, [query]);
 
   function handleSelectMovie(id) {
